59c7d5cbcc170f7e0959043e72ed5f27a94a4218
MatheusVict <matheusvictorhenrique@gmail.com>
2022-10-17 11:02:24 -0300

    Use Poké Api

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const maxPokes = 251;
+
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokes}`);
+
+  const data = await res.json();
+
+  data.results.forEach((item, index) => {
+
+    item.id = index + 1;
+
+  })
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+
+}
+
+export default function Home({ pokemons }) {
   return (
-    <div className={styles.container}>
-     
-    </div>
+    <>
+      <ul>
+        {pokemons.map((pokemon) => {
+          return <li key={pokemon.id}>{pokemon.name}</li>
+        })}
+      </ul>
+    </>
   )
 }
